0a9e51d512495e8698090aace9c1822a26fbeac6
Shevja <vlsdki@gmail.com>
2022-08-14 19:09:10 +0700

    added stlyles for planet element

--- src/components/solarSystemModulationContainer.jsx
+++ src/components/solarSystemModulationContainer.jsx
@@ -5,31 +5,31 @@ import './assets/planet.css'
 const SolarSystemModulationContainer = () => {
     return (
         <div className="modulation-container">
-            <div id="sun"></div>
-            <div id="mercury-orbit" className="planet-orbit">
-                <div id="mercury" className="planet"></div>
-            </div>
-            <div id="venus-orbit" className="planet-orbit">
-                <div id="venus" className="planet"></div>
-            </div>
-            <div id="earth-orbit" className="planet-orbit">
-                <div id="earth" className="planet"></div>
-            </div>
-            <div id="mars-orbit" className="planet-orbit">
-                <div id="mars" className="planet"></div>
-            </div>
-            <div id="jupiter-orbit" className="planet-orbit">
-                <div id="jupiter" className="planet"></div>
-            </div>
-            <div id="saturn-orbit" className="planet-orbit">
-                <div id="saturn" className="planet"></div>
-            </div>
-            <div id="uranus-orbit" className="planet-orbit">
-                <div id="uranus" className="planet"></div>
-            </div>
-            <div id="neptune-orbit" className="planet-orbit">
+            <button id="neptune-orbit" className="planet-orbit">
                 <div id="neptune" className="planet"></div>
-            </div>
+            </button>
+            <button id="uranus-orbit" className="planet-orbit">
+                <div id="uranus" className="planet"></div>
+            </button>
+            <button id="saturn-orbit" className="planet-orbit">
+                <div id="saturn" className="planet"></div>
+            </button>
+            <button id="jupiter-orbit" className="planet-orbit">
+                <div id="jupiter" className="planet"></div>
+            </button>
+            <button id="mars-orbit" className="planet-orbit">
+                <div id="mars" className="planet"></div>
+            </button>
+            <button id="earth-orbit" className="planet-orbit">
+                <div id="earth" className="planet"></div>
+            </button>
+            <button id="venus-orbit" className="planet-orbit">
+                <div id="venus" className="planet"></div>
+            </button>
+            <button id="mercury-orbit" className="planet-orbit">
+                <div id="mercury" className="planet"></div>
+            </button>
+            <div id="sun"></div>
         </div>
     );
 }
